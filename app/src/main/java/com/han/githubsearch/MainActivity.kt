e05a6859984afc8ef9434ecd603220bf8ab351f2
hanlee <ian.stmaster@gmail.com>
2021-04-05 01:57:44 +0900

    view 세팅

--- app/src/main/java/com/han/githubsearch/MainActivity.kt
+++ app/src/main/java/com/han/githubsearch/MainActivity.kt
@@ -2,10 +2,19 @@ package com.han.githubsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.han.githubsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMainBinding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.search.setOnClickListener {
+            val keyword = activityMainBinding.keyword.text.toString()
+            Log.d(javaClass.simpleName, "keyword = $keyword")
+        }
     }
 }
